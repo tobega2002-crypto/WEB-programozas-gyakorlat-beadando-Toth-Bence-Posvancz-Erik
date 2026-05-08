82ad98a37f6d1c325dba120aafd4b1b48b1db603
tobega2002 <tobega2002@gmail.com>
2026-05-08 16:05:08 +0200

    Update belep.php

--- logicals/belep.php
+++ logicals/belep.php
@@ -2,8 +2,7 @@
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
-                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        global $dbh;
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Felhsználó keresése
