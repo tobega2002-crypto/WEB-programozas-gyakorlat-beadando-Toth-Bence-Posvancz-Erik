7ea0d9f52f950818c1ce94e44f9d87dbf0b320ce
tobega2002 <tobega2002@gmail.com>
2026-05-08 15:18:32 +0200

    Update regisztral.php

--- logicals/regisztral.php
+++ logicals/regisztral.php
@@ -2,8 +2,7 @@
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo']) && isset($_POST['vezeteknev']) && isset($_POST['utonev'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
-                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        global $dbh; 
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Létezik már a felhasználói név?
